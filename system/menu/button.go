8b625d240a7878c7cb74fbce9ef488857dd0a6a1
Federico Devigili <federicodevigili@gmail.com>
2025-10-10 17:20:51 +0200

    refactor(menu): Expose Button's action function

--- system/menu/button.go
+++ system/menu/button.go
@@ -3,16 +3,22 @@ package menu
 type Button struct {
 	BaseItem // embeds parent/children logic
 	label    string
-	action   func()
+	Action   func()
 }
 
+// NewButton creates a new Button
 func NewButton(label string, action func()) *Button {
-	return &Button{label: label, action: action}
+	return &Button{
+		label:  label,
+		Action: action,
+	}
 }
 
 func (b *Button) Label() string { return b.label }
+
+// Update executes the action when selected
 func (b *Button) Update(cmd Command) {
-	if cmd == CmdSelect && b.action != nil {
-		b.action()
+	if cmd == CmdSelect && b.Action != nil {
+		b.Action()
 	}
 }

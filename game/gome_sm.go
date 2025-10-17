45cb46472b12744eee1261286ec472744e447e0e
Federico Devigili <federicodevigili@gmail.com>
2025-10-17 16:57:57 +0200

    Adds basic game states and transitions
    Defines core game states (Gameplay, Paused, FinishGame) and
    transitions between them to establish the basic game flow.
    
    This enhancement allows for a more structured and manageable
    game state, enabling features like pausing, resuming, and
    ending the game.

--- game/gome_sm.go
+++ game/gome_sm.go
@@ -5,28 +5,56 @@ import (
 )
 
 type GomeFSM struct {
-	FSM         *sm.StateMachine
+	FSM *sm.StateMachine
+
+	// States
 	Initial     *sm.State
 	OptionsMenu *sm.State
 	GameStart   *sm.State
+	Gameplay    *sm.State
+	Paused      *sm.State
+	FinishGame  *sm.State
 
-	// Exposed transitions
+	// Transitions
 	InitialToOptions *sm.Transition
 	OptionsToGame    *sm.Transition
+	GameStartToPlay  *sm.Transition
+	PlayToPause      *sm.Transition
+	PauseToPlay      *sm.Transition
+	PlayToFinish     *sm.Transition
+	FinishToInitial  *sm.Transition
 }
 
 func NewGomeFSM() *GomeFSM {
 	fsm := sm.NewStateMachine()
 
+	// Define states
 	initial := sm.NewState("Initial")
 	options := sm.NewState("OptionsMenu")
 	gameStart := sm.NewState("GameStart")
+	gameplay := sm.NewState("Gameplay")
+	paused := sm.NewState("Paused")
+	finish := sm.NewState("FinishGame")
 
+	// Define transitions
 	initialToOptions := sm.NewTransition(initial, options, nil, "InitToOptions")
 	optionsToGame := sm.NewTransition(options, gameStart, nil, "StartGame")
+	gameStartToPlay := sm.NewTransition(gameStart, gameplay, nil, "StartGameplay")
+	playToPause := sm.NewTransition(gameplay, paused, nil, "PauseGame")
+	pauseToPlay := sm.NewTransition(paused, gameplay, nil, "ResumeGame")
+	playToFinish := sm.NewTransition(gameplay, finish, nil, "FinishGame")
+	finishToInitial := sm.NewTransition(finish, initial, nil, "ReturnToMenu")
 
+	// Register transitions
 	fsm.AddTransition(initialToOptions)
 	fsm.AddTransition(optionsToGame)
+	fsm.AddTransition(gameStartToPlay)
+	fsm.AddTransition(playToPause)
+	fsm.AddTransition(pauseToPlay)
+	fsm.AddTransition(playToFinish)
+	fsm.AddTransition(finishToInitial)
+
+	// Set initial state
 	fsm.SetState(initial)
 
 	return &GomeFSM{
@@ -34,7 +62,15 @@ func NewGomeFSM() *GomeFSM {
 		Initial:          initial,
 		OptionsMenu:      options,
 		GameStart:        gameStart,
+		Gameplay:         gameplay,
+		Paused:           paused,
+		FinishGame:       finish,
 		InitialToOptions: initialToOptions,
 		OptionsToGame:    optionsToGame,
+		GameStartToPlay:  gameStartToPlay,
+		PlayToPause:      playToPause,
+		PauseToPlay:      pauseToPlay,
+		PlayToFinish:     playToFinish,
+		FinishToInitial:  finishToInitial,
 	}
 }
